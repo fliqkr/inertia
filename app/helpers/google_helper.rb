43a2700c18ff9edf5976978c70170e557f529dbf
fliqkr <fliqkr@simswap.in>
2023-05-13 00:45:08 +0200

    (feat): Link widgets, better styles

--- app/helpers/google_helper.rb
+++ app/helpers/google_helper.rb
@@ -27,20 +27,102 @@ module GoogleHelper
     # You can view the current DOM layout of Google by just searching
     # for something and exploring it with devtools.
     dom.css('div.g').each do |item|
+      # Result Properties -->
+
       # Select the result title, description and link
       title_elem = item.css('h3').first
       link_elem = item.css('a').first
       description_elem = item.css('div.VwiC3b').first
 
+      # Get the content
       title = title_elem.text.strip if title_elem
       link = link_elem['href'].sub(/^\/url\?q=/, '').split('&').first if link_elem
       description = description_elem.text.strip if description_elem
 
+      # Result Widgets -->
+      sublinks = get_sublinks(item)
+      rich_content_object = get_rich_content_object(item)
+
       # Add it to our results
-      results << { title: title, link: link, description: description }
+      results << {
+        title: title,
+        link: link,
+        description: description,
+        sublinks: sublinks,
+        rich_content_object: rich_content_object
+      }
     end
 
     # Return the results
     results
+  end
+
+  # Returns sublinks that exist within a page
+  def get_sublinks(item)
+    # Get the parent table
+    parent = item.css('table.jmjoTe').first
+    # Table elements
+    links = item.css('tr.mslg')
+
+    sublinks = []
+
+    # Loop over all the table elements
+    links.each do |link|
+      # Select the sublink title, description and link
+      title_elem = link.css('h3').first
+      link_elem = link.css('a').first
+      description_elem = item.css('div.zz3gNc').first
+
+      # Get the content
+      title = title_elem.text.strip if title_elem
+      link = link_elem['href'].sub(/^\/url\?q=/, '').split('&').first if link_elem
+      description = description_elem.text.strip if description_elem
+
+      sublinks << { title: title, link: link, description: description }
+    end
+
+    # Return the sublinks
+    sublinks
+  end
+
+  # Returns key-value based rich content, e.g. the amount of edits on Wikipedia
+  def get_rich_content_object(item)
+    # Get the parent div
+    parent = item.css('div.Z26q7c').first
+    # Object fields
+    fields = item.css('div.wFMWsc')
+
+    rich_content = []
+
+    fields.each do |field|
+      key_elem = field.css('span.YrbPuc').first
+      value_elem = field.css('span.wHYlTd').first
+
+      # Check what type the value is (text/link)
+      value_link_elem = value_elem.css('a').first
+      value_type =
+        if !value_link_elem.nil?
+          :link
+        else
+          :text
+        end
+
+      value_content =
+        if value_type == :link
+          label = value_link_elem.text.strip if value_link_elem
+          link = value_link_elem['href'].sub(/^\/url\?q=/, '').split('&').first if value_link_elem
+          "#{label}|#{link}"
+        elsif value_type == :text
+          value_elem.text.strip
+        end
+
+      # Get the content
+      key = key_elem.text.strip if key_elem
+      value = { type: value_type, content: value_content }
+
+      rich_content << { key: key, value: value }
+    end
+
+    rich_content
   end
 end
